58b66b923bf906b56197716f3de7daf5ebe244b6
HEA <hea_s@naver.com>
2022-10-04 15:37:19 +0900

    PHP

--- chapter4/create.php
+++ chapter4/create.php
@@ -19,13 +19,17 @@
             ?>     
         </ol>
         <a href="create.php">create</a> 
-        <?php if(isset($_GET['id'])) { ?>
-            <a href="update.php?id=<?= $_GET['id']; ?>">update</a>
-            <form action="delete_process.php" method="post">
-                <input type="hidden" name="id" value="<?= $_GET['id'] ?>" />
-                <input type="submit" value="delete" />
-            </form>
-        <?php } ?>
+        <form action="create_process.php" method="post">
+            <p>
+                <input type="text" name="title" placeholder="Title" />
+            </p>
+            <p>
+                <textarea name="description" placeholder="Description"></textarea>
+            </p>
+            <p>
+                <input type="submit" value="Submit" />
+            </p>
+        </form>    
         <h2>
             <?php
                 print_title();
