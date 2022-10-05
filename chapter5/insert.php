ed1427544d3f4a65f19db218b0120053983c45cb
HEA <hea_s@naver.com>
2022-10-05 17:06:38 +0900

    SQL

--- chapter5/insert.php
+++ chapter5/insert.php
@@ -1,14 +1,14 @@
 <?php
-    $mysqli = mysqli_connect("localhost", "root", "koreait", "tutorials");
-    //var_dump($mysqli);
-    $res = mysqli_query($mysqli, "SHOW TABLES");
-    //var_dump($res);
-    if(!$res) {
-        echo mysqli_error($mysqli);
-        exit;
-    }
-
-    $row = mysqli_fetch_assoc($res);
-    var_dump($row); 
-    //$res = $mysqli->query("SHOW TABLES");
+    $conn = mysqli_connect("localhost", "root", "koreait", "tutorials");
+    $sql = "
+        INSERT INTO topic (
+            title,
+            description,
+            created
+        ) VALUES (
+            'My SQL',
+            'My SQL is...',
+            NOW()
+        )";
+    mysqli_query($conn, $sql); 
 ?>
